e1a2c4eee081fbde93e707fc2547b2c784d9d317
Ashwin Chandrasekar <ashw.chandr@gmail.com>
2023-03-04 15:15:42 -0500

    Remove Google provider version constraints

--- main.tf
+++ main.tf
@@ -2,19 +2,17 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "4.44.1"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "4.44.1"
     }
     kubectl = {
       source  = "gavinbunney/kubectl"
-      version = "1.14.0"
+      version = ">=1.14.0"
     }
     helm = {
       source  = "hashicorp/helm"
-      version = "2.7.1"
+      version = ">=2.7.1"
     }
   }
 }
